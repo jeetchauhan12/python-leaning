d280c83994ef0da6ba838901c73d22f127d0ea71
jeetchauhan12 <chauhanjeet419@gmail.com>
2026-01-13 13:00:16 +0530

    Phython Basics: varibale declration and other

---
+++ python-basics.py
@@ -0,0 +1,38 @@
+a = 10    # interger example
+b = -5
+c = 0
+
+print(type(c))
+print(a + b)
+
+
+a = 3.67  # float example
+b = 78.45
+d = 0
+print(type(d))
+print(b-a)
+
+
+z = 2 + 3j  # Complex Number Example 
+
+print(type(z))
+print(z.real)
+print(z.imag)
+
+
+name = "Python" # String Example 
+
+print(type(name))
+print(name.upper())
+
+
+hello = "I love you python " # String Length Example
+zero ="" # empty string example
+print (len(hello))
+print(zero)
+
+a = True   # Boolean Example
+b = False
+
+print(type(a))
+print(a or b)
